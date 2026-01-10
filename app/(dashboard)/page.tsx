f7fe6543e5cd96a8d041357582703a5156a448e7
Bruni <bruno.a.dinis96@gmail.com>
2026-01-10 18:00:25 -0300

    feat: add initial revenue chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -16,14 +16,21 @@ import {
   SummaryCardTitle,
   SummaryCardValue,
 } from "./_components/summary-card";
-import { getDashboardData } from "../_actions/dashboard/get-dashboard";
+import { getDashboardData } from "../_data-access/dashboard/get-dashboard";
 import { formatCurrency } from "../_helpers/currency";
+import RevenueChart from "./_components/revenue-chart";
 
 const Home = async () => {
-  const { todayRevenue, totalRevenue, totalProducts, totalSales, totalStock } =
-    await getDashboardData();
+  const {
+    todayRevenue,
+    totalRevenue,
+    totalProducts,
+    totalSales,
+    totalStock,
+    totalLast14DaysRevenue,
+  } = await getDashboardData();
   return (
-    <div className="mx-8 my-8 w-full space-y-8 rounded-lg">
+    <div className="mx-8 my-8 flex w-full flex-col space-y-8 rounded-lg">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Visão geral</HeaderSubtitle>
@@ -68,6 +75,12 @@ const Home = async () => {
           <SummaryCardTitle>Produtos</SummaryCardTitle>
           <SummaryCardValue>{totalProducts}</SummaryCardValue>
         </SummaryCard>
+      </div>
+      <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
+        <p className="text-lg text-sm font-semibold text-slate-900">Receita </p>
+        <p className="text-sm text-slate-400">Últimos 14 dias</p>
+
+        <RevenueChart data={totalLast14DaysRevenue} />
       </div>
     </div>
   );
